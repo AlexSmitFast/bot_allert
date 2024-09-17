7fce3393a748720638af4769bd435840f2f59c60
AlexSmitFast <97442052+AlexSmitFast@users.noreply.github.com>
2024-09-17 23:35:23 +0300

    Add files via upload

---
+++ startunit.py
@@ -0,0 +1,7 @@
+SANDBOX_TOKEN = 't.DjIIVBMV_uuswHNipuX9mmy-f_Rpr-V2_XLA9b9Hiauyjt-Uuxf_y5qBQ5glPj81kRZYyPU1j9dUr_CdWBcdjQ'
+TOKEN = "t.1rkLvPpiyc5j3yTPpix3yMjAdYJRCb1KhfFooO9Ilj6hIR63ih9Hd_HAEOblXMQfONfdotYXyO0IrMNcUnb_-g"
+TG_PA1_TOKEN ='1401332930:AAHm12ywR4c4qqg-K2-MwexG_NsCNn09AZw' # https://t.me/AlertPriceTXTbot
+TG_PA2_TOKEN = '6510816769:AAFndA_fWdIGe9ODkmqYGEPcW-AhAwHep1s'  # https://t.me/Allert_rezerv_bot
+TG_TOKEN_T_L ='5334651915:AAGo5-Dfl4JFP4HA2VF2d31sgb_ggU51qBE' # https://t.me/ListOfTaskBot
+TG_TOKEN_ERR_BOT = '6840698106:AAHi1Sjh94K3PNaaD8ijz_7AJS54U2_7NyQ'  # https://t.me/telega_error_allert_bot
+TG_GRAFIK_POST_BOT = '6560755225:AAHIJYVN9xu-w3CJwzU4tFK4YUmh8CVVtxk' # https://t.me/grafiks_post_bot
